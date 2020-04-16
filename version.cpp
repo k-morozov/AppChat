322f2f5e37fdb6e910362389b53420b4a894e981
Konstantin Morozov <34001730+1FoCuS@users.noreply.github.com>
2020-04-16 19:35:16 +0300

    Add files via upload

---
+++ version.cpp
@@ -0,0 +1,9 @@
+#include "version.h"
+
+namespace test {
+    int GetVersion()
+    {
+        return 42;
+    }
+
+} // namespace name
---
+++ version.h
@@ -0,0 +1,9 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+namespace test {
+    int GetVersion();
+
+} // namespace name
+
+#endif // VERSION_H
