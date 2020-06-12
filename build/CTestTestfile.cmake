282e1b02687cce7ee082a5f8f433d4f290c64a24
1focus <scfocus@yandex.ru>
2020-06-12 19:07:36 +0300

    del files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/focus/Workspace/Development/App-chat
-# Build directory: /home/focus/Workspace/Development/App-chat/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/qtcsettings.cmake
+++
@@ -1,7 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/home/focus/Qt/5.14.1/gcc_64" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/home/focus/Qt/5.14.1/gcc_64/bin/qmake" CACHE "STRING" "" FORCE)
