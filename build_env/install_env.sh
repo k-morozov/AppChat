7364cc1240fbf6c82afb90c87d00491d8273e0a8
dima <dimalosev0605@gmail.com>
2020-07-13 13:41:39 +0300

    Small changes

--- build_env/install_env.sh
+++ build_env/install_env.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#! /bin/bash
 
 # install boost
 BOOST_VER=73
 BOOST=boost_1_${BOOST_VER}_0
 #wget https://sourceforge.net/projects/boost/files/boost/1.${BOOST_VER}.0/${BOOST}.tar.gz || return 1
-#wget https://dl.bintray.com/boostorg/release/1.${BOOST_VER}.0/source/boost_1_${BOOST_VER}_0.tar.gz || return 1
-cd ${BOOST}
+wget https://dl.bintray.com/boostorg/release/1.${BOOST_VER}.0/source/boost_1_${BOOST_VER}_0.tar.gz || return 1
+tar xzf ${BOOST}.tar.gz && cd ${BOOST}
 ./bootstrap.sh || return 1
 cat <<EOF > ./project-config.jam
 using gcc : : ${CXX} ;
