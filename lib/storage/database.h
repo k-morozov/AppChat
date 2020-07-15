a519d4ee92217f6710cca98b16fe7b06d1ba3c63
tuminskiy <uzvername@gmail.com>
2020-07-16 02:10:08 +0300

    refactoring database

--- lib/storage/database.h
+++ lib/storage/database.h
@@ -6,13 +6,13 @@
 #include <sqlite3.h>
 #include <logger.h>
 
+#include <memory>
+
 class Database
 {
 public:
-    static Database& Instance() {
-        static Database storage;
-        return storage;
-    }
+    Database(const std::string& _db_name);
+    ~Database();
 
     void save_text_message(text_request_ptr message);
     std::deque<text_response_ptr> load_history(identifier_t roomid);
@@ -24,10 +24,7 @@ public:
 
     identifier_t check_client(std::string login, std::string password) const;
 
-    ~Database();
 private:
-    Database();
-
     const std::string db_name = "history.db";
     sqlite3* db_ptr;
     static std::string create_table_history;
@@ -35,5 +32,7 @@ private:
 
     log4cplus::Logger logger = LOGGER("Database");
 };
+
+using database_ptr = std::shared_ptr<Database>;
 
 #endif // DATABASE_H
