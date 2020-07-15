487e605237c3daacf7eea11edf2a1d44b9bdc284
Alexey Minimulin <a.s.minimulin@gmail.com>
2020-07-15 13:18:37 +0300

    Add docs to client app

--- src/client/client/client.h
+++ src/client/client/client.h
@@ -8,10 +8,22 @@
 #include <protocol.h>
 #include <QWidget>
 
+/**
+ * @brief Client class
+ * 
+ * @details Implement communication with server
+ */
 class Client: public QObject {
    Q_OBJECT
 
 public:
+    /**
+     * @brief Construct a new Client object
+     * 
+     * @param io boost::asio::io_service
+     * @param eps 
+     * @param request initial request the server
+     */
     Client(boost::asio::io_service &io, const boost::asio::ip::tcp::resolver::results_type& eps,
            input_request_ptr request)
         : io_service(io), sock(io), eps(eps)
@@ -19,21 +31,51 @@ public:
         do_connect(eps, request);
     }
 
+    /**
+     * @brief Send text message
+     * 
+     * @param message 
+     */
     void write(const std::string& message);
+
+    /**
+     * @brief Send text request
+     */
     void write(text_request_ptr);
+
+    /**
+     * @brief Send join room request
+     */
     void write(join_room_request_ptr);
 
+    /**
+     * @brief Finish the communication with server
+     */
     void close() {
         boost::asio::post(io_service, [this]() {
             sock.close();
         });
     }
+
+    /**
+     * @brief Setter for client_id
+     * 
+     * @param id 
+     */
     void set_login_id(identifier_t id)   { client_id = id;}
+
+    /**
+     * @brief Login getter
+     * 
+     * @return const char* 
+     */
     const char* get_login() const { return login; }
 
-    ~Client() {
+    /**
+     * @brief Default destructor
+     */
+    ~Client() = default;
 
-    }
 private:
     boost::asio::io_service &io_service;
     boost::asio::ip::tcp::socket sock;
@@ -48,23 +90,73 @@ private:
     identifier_t room_id = 0;
 
 private:
+    /**
+     * @brief Log in scenario implementation
+     * 
+     * @return input_request_ptr 
+     */
     input_request_ptr logon();
+
+    /**
+     * @brief Start connection to the server
+     */
     void do_connect(const boost::asio::ip::tcp::resolver::results_type&, input_request_ptr);
 
+    /**
+     * @brief Log in on the server
+     * 
+     * @param packet 
+     */
     void send_login_packet(packet_ptr packet);
 
+    /**
+     * @brief Entry point for handling server response
+     */
     void read_response_header();
+
+    /**
+     * @brief Handle registartion response
+     */
     void read_response_data(registr_response_ptr);
+
+    /**
+     * @brief Handle authorization response
+     * TODO: fix typo autor -> authorization
+     */
     void read_response_data(autor_response_ptr);
+
+    /**
+     * @brief Handle text message response
+     * 
+     */
     void read_response_data(text_response_ptr);
 
+    /**
+     * @brief Entry point to actually send request
+     */
     void send_request_header();
+
+    /**
+     * @brief Send request data to server
+     * 
+     * @note Do not call it manually,
+     * it must be called from the @link Client::send_request_header "send_reqest_header".
+     */
     void send_request_data();
 
 
 signals:
+    /**
+     * @brief 
+     * 
+     * @param from sender's login
+     * @param text 
+     */
     void send_text(const std::string& from, const std::string& text);
 
+    /**
+     * @brief send input code
+     */
     void send_input_code(InputCode);
 //    void good_input();
 };
