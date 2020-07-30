4e7080ab28075f3ecff34366de26a9fd0127ad4c
k-morozov <scfocus@yandex.ru>
2020-07-30 11:15:01 +0300

    fix load

--- scripts/install_env.sh
+++ scripts/install_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 install_boost() {
+    echo "install boost library from taz.gz"
     wget -O boost_1_73_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.73.0/boost_1_73_0.tar.gz/download
     tar -xf boost_1_73_0.tar.gz
     cd boost_1_73_0
@@ -9,13 +10,13 @@ install_boost() {
 }
 
 sudo apt-get update
-sudo apt-get install cmake
-sudo apt-get install libgtest-dev
-sudo apt-get install sqlite3 libsqlite3-dev
-sudo apt-get install doxygen
-sudo apt-get install -qq doxygen graphviz
-sudo apt-get install libboost-test-dev
-sudo apt install qt5-default
+#sudo apt-get install cmake
+#sudo apt-get install libgtest-dev
+#sudo apt-get install sqlite3 libsqlite3-dev
+#sudo apt-get install doxygen
+#sudo apt-get install -qq doxygen graphviz
+#sudo apt-get install libboost-test-dev
+#sudo apt install qt5-default
 
 install_boost
 
