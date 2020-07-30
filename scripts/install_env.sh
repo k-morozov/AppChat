97941b4f21d20d89fa9c386d9170bdbff6311331
k-morozov <scfocus@yandex.ru>
2020-07-30 10:55:15 +0300

    fix load

--- scripts/install_env.sh
+++ scripts/install_env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 install_boost() {
-    echo "install boost library from taz.gz"
     wget -O boost_1_73_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.73.0/boost_1_73_0.tar.gz/download
     tar -xf boost_1_73_0.tar.gz
     cd boost_1_73_0
