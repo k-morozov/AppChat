a9b15ed66c8789afdb6a555ce9322ef6750ce66c
k-morozov <scfocus@yandex.ru>
2020-08-06 08:21:49 +0300

    add boost::test

--- src/tests/test_datetime.cpp
+++ src/tests/test_datetime.cpp
@@ -1,205 +1,195 @@
-//#include <gtest/gtest.h>
+#include <boost/test/unit_test.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include "datetime.h"
 
-//#include <boost/date_time/posix_time/posix_time.hpp>
+/**
+ * @brief Unit tests for AppChat project.
+ */
+BOOST_AUTO_TEST_SUITE(test_request)
 
-//#include "datetime.h"
+BOOST_AUTO_TEST_CASE(construction_by_default) {
+    DateTime dt;
 
-//class datetime : public ::testing::Test
-//{
-//protected:
-//    virtual void SetUp() override { }
-//    virtual void TearDown() override { }
-//};
+    BOOST_CHECK(dt.hours == 0);
+    BOOST_CHECK(dt.minutes == 0);
+    BOOST_CHECK(dt.seconds == 0);
+    BOOST_CHECK(dt.day == 0);
+    BOOST_CHECK(dt.month == 0);
+    BOOST_CHECK(dt.year == 0);
+}
 
-//TEST_F(datetime, construction_by_default)
-//{
-//    DateTime dt;
+BOOST_AUTO_TEST_CASE(construction_from_ptime) {
+    using namespace boost::posix_time;
 
-//    EXPECT_EQ(dt.hours, 0);
-//    EXPECT_EQ(dt.minutes, 0);
-//    EXPECT_EQ(dt.seconds, 0);
+    ptime pt = second_clock::universal_time();
 
-//    EXPECT_EQ(dt.day, 0);
-//    EXPECT_EQ(dt.month, 0);
-//    EXPECT_EQ(dt.year, 0);
-//}
+    DateTime dt(pt);
 
-//TEST_F(datetime, construction_from_ptime)
-//{
-//    using namespace boost::posix_time;
+    BOOST_CHECK(dt.hours == pt.time_of_day().hours());
+    BOOST_CHECK(dt.minutes == pt.time_of_day().minutes());
+    BOOST_CHECK(dt.seconds == pt.time_of_day().seconds());
 
-//    ptime pt = second_clock::universal_time();
+    BOOST_CHECK(dt.day == pt.date().day());
+    BOOST_CHECK(dt.month == pt.date().month());
+    BOOST_CHECK(dt.year + 2000 == pt.date().year());
+}
 
-//    DateTime dt(pt);
-
-//    EXPECT_EQ(dt.hours, pt.time_of_day().hours());
-//    EXPECT_EQ(dt.minutes, pt.time_of_day().minutes());
-//    EXPECT_EQ(dt.seconds, pt.time_of_day().seconds());
-
-//    EXPECT_EQ(dt.day, pt.date().day());
-//    EXPECT_EQ(dt.month, pt.date().month());
-//    EXPECT_EQ(dt.year + 2000, pt.date().year());
-//}
-
-//TEST_F(datetime, simple_date_string)
-//{
-//    DateTime dt;
+BOOST_AUTO_TEST_CASE(simple_date_string) {
+    DateTime dt;
     
-//    dt.day = 22;
-//    dt.month = 7;
-//    dt.year = 20;
+    dt.day = 22;
+    dt.month = 7;
+    dt.year = 20;
 
-//    EXPECT_EQ(dt.to_simple_date(), "22.07.2020");
+    BOOST_CHECK(dt.to_simple_date() == "22.07.2020");
 
-//    dt.day = 0;
-//    dt.month = 0;
-//    dt.year = 0;
+    dt.day = 0;
+    dt.month = 0;
+    dt.year = 0;
 
-//    EXPECT_EQ(dt.to_simple_date(), "00.00.2000");
+    BOOST_CHECK(dt.to_simple_date() == "00.00.2000");
 
-//    dt.day = 31;
-//    dt.month = 12;
-//    dt.year = 255;
+    dt.day = 31;
+    dt.month = 12;
+    dt.year = 255;
 
-//    EXPECT_EQ(dt.to_simple_date(), "31.12.2255");
-//}
+    BOOST_CHECK(dt.to_simple_date() == "31.12.2255");
+}
 
-//TEST_F(datetime, simple_time_string)
-//{
-//    DateTime dt;
+BOOST_AUTO_TEST_CASE(simple_time_string) {
+    DateTime dt;
 
-//    dt.hours = 16;
-//    dt.minutes = 5;
-//    dt.seconds = 50;
+    dt.hours = 16;
+    dt.minutes = 5;
+    dt.seconds = 50;
 
-//    EXPECT_EQ(dt.to_simple_time(), "16:05:50");
+    BOOST_CHECK(dt.to_simple_time() == "16:05:50");
 
-//    dt.hours = 0;
-//    dt.minutes = 0;
-//    dt.seconds = 0;
+    dt.hours = 0;
+    dt.minutes = 0;
+    dt.seconds = 0;
 
-//    EXPECT_EQ(dt.to_simple_time(), "00:00:00");
+    BOOST_CHECK(dt.to_simple_time() == "00:00:00");
 
-//    dt.hours = 23;
-//    dt.minutes = 59;
-//    dt.seconds = 59;
+    dt.hours = 23;
+    dt.minutes = 59;
+    dt.seconds = 59;
 
-//    EXPECT_EQ(dt.to_simple_time(), "23:59:59");
-//}
+    BOOST_CHECK(dt.to_simple_time() == "23:59:59");
+}
 
-//TEST_F(datetime, operator_equal)
-//{
-//    DateTime lhs;
+BOOST_AUTO_TEST_CASE(operator_equal) {
+    DateTime lhs;
 
-//    lhs.hours = 1;
-//    lhs.minutes = 2;
-//    lhs.seconds = 3;
+    lhs.hours = 1;
+    lhs.minutes = 2;
+    lhs.seconds = 3;
 
-//    lhs.day = 4;
-//    lhs.month = 5;
-//    lhs.year = 6;
+    lhs.day = 4;
+    lhs.month = 5;
+    lhs.year = 6;
 
-//    DateTime rhs;
+    DateTime rhs;
 
-//    rhs.hours = 1;
-//    rhs.minutes = 2;
-//    rhs.seconds = 3;
+    rhs.hours = 1;
+    rhs.minutes = 2;
+    rhs.seconds = 3;
 
-//    rhs.day = 4;
-//    rhs.month = 5;
-//    rhs.year = 6;
+    rhs.day = 4;
+    rhs.month = 5;
+    rhs.year = 6;
 
-//    EXPECT_EQ(lhs == rhs, true);
+    BOOST_CHECK(lhs == rhs);
 
-//    rhs.year = 7;
+    rhs.year = 7;
 
-//    EXPECT_EQ(lhs == rhs, false);
+    BOOST_CHECK(!(lhs == rhs));
 
-//    rhs.year = 5;
+    rhs.year = 5;
 
-//    EXPECT_EQ(lhs == rhs, false);
-//}
+    BOOST_CHECK(!(lhs == rhs));
+}
 
-//TEST_F(datetime, operator_less)
-//{
-//    DateTime lhs;
+BOOST_AUTO_TEST_CASE(operator_less) {
+    DateTime lhs;
 
-//    lhs.seconds = 1;
-//    lhs.minutes = 2;
-//    lhs.hours = 3;
+    lhs.seconds = 1;
+    lhs.minutes = 2;
+    lhs.hours = 3;
 
-//    lhs.day = 4;
-//    lhs.month = 5;
-//    lhs.year = 6;
+    lhs.day = 4;
+    lhs.month = 5;
+    lhs.year = 6;
 
-//    DateTime rhs;
+    DateTime rhs;
 
-//    rhs.seconds = 1;
-//    rhs.minutes = 2;
-//    rhs.hours = 3;
+    rhs.seconds = 1;
+    rhs.minutes = 2;
+    rhs.hours = 3;
 
-//    rhs.day = 4;
-//    rhs.month = 5;
-//    rhs.year = 6;
+    rhs.day = 4;
+    rhs.month = 5;
+    rhs.year = 6;
 
-//    EXPECT_EQ(lhs < rhs, false);
+    BOOST_CHECK(!(lhs < rhs));
 
-//    lhs.year = 7;
+    lhs.year = 7;
 
-//    EXPECT_EQ(lhs < rhs, false);
+    BOOST_CHECK(!(lhs < rhs));
 
-//    lhs.year = 6;
-//    lhs.month = 6;
+    lhs.year = 6;
+    lhs.month = 6;
 
-//    EXPECT_EQ(lhs < rhs, false);
+    BOOST_CHECK(!(lhs < rhs));
 
-//    lhs.month = 5;
-//    lhs.day = 5;
+    lhs.month = 5;
+    lhs.day = 5;
 
-//    EXPECT_EQ(lhs < rhs, false);
+    BOOST_CHECK(!(lhs < rhs));
 
-//    lhs.day = 4;
-//    lhs.hours = 4;
+    lhs.day = 4;
+    lhs.hours = 4;
 
-//    EXPECT_EQ(lhs < rhs, false);
+    BOOST_CHECK(!(lhs < rhs));
 
-//    lhs.hours = 3;
-//    lhs.minutes = 3;
+    lhs.hours = 3;
+    lhs.minutes = 3;
 
-//    EXPECT_EQ(lhs < rhs, false);
+    BOOST_CHECK(!(lhs < rhs));
 
-//    lhs.minutes = 2;
-//    lhs.seconds = 2;
+    lhs.minutes = 2;
+    lhs.seconds = 2;
 
-//    EXPECT_EQ(lhs < rhs, false);
+    BOOST_CHECK(!(lhs < rhs));
 
-//    lhs.seconds = 0;
+    lhs.seconds = 0;
 
-//    EXPECT_EQ(lhs < rhs, true);
+    BOOST_CHECK(lhs < rhs);
 
-//    lhs.seconds = 1;
-//    lhs.minutes = 1;
+    lhs.seconds = 1;
+    lhs.minutes = 1;
 
-//    EXPECT_EQ(lhs < rhs, true);
+    BOOST_CHECK(lhs < rhs);
 
-//    lhs.minutes = 2;
-//    lhs.hours = 2;
+    lhs.minutes = 2;
+    lhs.hours = 2;
 
-//    EXPECT_EQ(lhs < rhs, true);
+    BOOST_CHECK(lhs < rhs);
 
-//    lhs.hours = 3;
-//    lhs.day = 3;
+    lhs.hours = 3;
+    lhs.day = 3;
 
-//    EXPECT_EQ(lhs < rhs, true);
+    BOOST_CHECK(lhs < rhs);
 
-//    lhs.day = 4;
-//    lhs.month = 4;
+    lhs.day = 4;
+    lhs.month = 4;
 
-//    EXPECT_EQ(lhs < rhs, true);
+    BOOST_CHECK(lhs < rhs);
 
-//    lhs.month = 5;
-//    lhs.year = 5;
+    lhs.month = 5;
+    lhs.year = 5;
 
-//    EXPECT_EQ(lhs < rhs, true);
-//}
+    BOOST_CHECK(lhs < rhs);
+}
 
+}
--- src/tests/test_response.cpp
+++ src/tests/test_response.cpp
@@ -1,66 +1,59 @@
-//#include <gtest/gtest.h>
-//#include <protocol.h>
-//#include <memory>
+#include <boost/test/unit_test.hpp>
 
-//class response: public ::testing::Test {
-//protected:
+#include <protocol.h>
+#include <memory>
 
-//    virtual void SetUp() override {
+/**
+ * @brief Unit tests for AppChat project.
+ */
+BOOST_AUTO_TEST_SUITE(test_request)
 
-//    }
-//    virtual void TearDown() override {
+BOOST_AUTO_TEST_CASE(response_registration_basic) {
+    identifier_t id = 12345;
+    RegistrationResponse  response(id);
 
-//    }
-//    virtual void TestBody() override {
+    BOOST_CHECK(response.get_protocol_version() == PROTOCOL_VERS);
+    BOOST_CHECK(response.get_type_data() == TypeCommand::RegistrationResponse);
+    BOOST_CHECK(response.get_loginid() == id);
+}
 
-//    }
-//};
+BOOST_AUTO_TEST_CASE(response_autorisation_basic) {
+    identifier_t id = 1234567890;
+    AutorisationResponse  response(id);
 
-//TEST_F(response, registration_basic) {
-//    int32_t id = 12345;
-//    RegistrationResponse  response(id);
+    BOOST_CHECK(response.get_protocol_version() == PROTOCOL_VERS);
+    BOOST_CHECK(response.get_type_data() == TypeCommand::AutorisationResponse);
+    BOOST_CHECK(response.get_loginid() == id);
+}
 
-//    EXPECT_EQ(response.get_protocol_version(), PROTOCOL_VERS);
-//    EXPECT_EQ(response.get_type_data(), TypeCommand::RegistrationResponse);
-//    EXPECT_EQ(response.get_loginid(), id);
-//}
+BOOST_AUTO_TEST_CASE(response_registration_basic_change_id) {
+    identifier_t id = 12345;
+    RegistrationResponse  response(id);
+    id = 123;
+    response.set_loginid(id);
+    BOOST_CHECK(response.get_protocol_version() == PROTOCOL_VERS);
+    BOOST_CHECK(response.get_type_data() == TypeCommand::RegistrationResponse);
+    BOOST_CHECK(response.get_loginid() == id);
 
-//TEST_F(response, autorisation_basic) {
-//    uint32_t id = 1234567890;
-//    AutorisationResponse  response(id);
+    response.set_loginid();
+    BOOST_CHECK(response.get_protocol_version() == PROTOCOL_VERS);
+    BOOST_CHECK(response.get_type_data() == TypeCommand::RegistrationResponse);
+    BOOST_CHECK(response.get_loginid() == 0);
+}
 
-//    EXPECT_EQ(response.get_protocol_version(), PROTOCOL_VERS);
-//    EXPECT_EQ(response.get_type_data(), TypeCommand::AutorisationResponse);
-//    EXPECT_EQ(response.get_loginid(), id);
-//}
+BOOST_AUTO_TEST_CASE(response_autorisation_basic_change_id) {
+    identifier_t id = 1234567890;
+    AutorisationResponse  response(id);
+    id = 123;
+    response.set_loginid(id);
 
-//TEST_F(response, registration_basic_change_id) {
-//    uint32_t id = 12345;
-//    RegistrationResponse  response(id);
-//    id = 123;
-//    response.set_loginid(id);
-//    EXPECT_EQ(response.get_protocol_version(), PROTOCOL_VERS);
-//    EXPECT_EQ(response.get_type_data(), TypeCommand::RegistrationResponse);
-//    EXPECT_EQ(response.get_loginid(), id);
+    BOOST_CHECK(response.get_protocol_version() == PROTOCOL_VERS);
+    BOOST_CHECK(response.get_type_data() == TypeCommand::AutorisationResponse);
+    BOOST_CHECK(response.get_loginid() == id);
 
-//    response.set_loginid();
-//    EXPECT_EQ(response.get_protocol_version(), PROTOCOL_VERS);
-//    EXPECT_EQ(response.get_type_data(), TypeCommand::RegistrationResponse);
-//    EXPECT_EQ(response.get_loginid(), 0);
-//}
-
-//TEST_F(response, autorisation_basic_change_id) {
-//    uint32_t id = 1234567890;
-//    AutorisationResponse  response(id);
-//    id = 123;
-//    response.set_loginid(id);
-
-//    EXPECT_EQ(response.get_protocol_version(), PROTOCOL_VERS);
-//    EXPECT_EQ(response.get_type_data(), TypeCommand::AutorisationResponse);
-//    EXPECT_EQ(response.get_loginid(), id);
-
-//    response.set_loginid();
-//    EXPECT_EQ(response.get_protocol_version(), PROTOCOL_VERS);
-//    EXPECT_EQ(response.get_type_data(), TypeCommand::AutorisationResponse);
-//    EXPECT_EQ(response.get_loginid(), 0);
-//}
+    response.set_loginid();
+    BOOST_CHECK(response.get_protocol_version() == PROTOCOL_VERS);
+    BOOST_CHECK(response.get_type_data() == TypeCommand::AutorisationResponse);
+    BOOST_CHECK(response.get_loginid() == 0);
+}
+}
