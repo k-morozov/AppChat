7a5f0511ceca6ee28f1114b8db9138358e2af20f
k-morozov <scfocus@yandex.ru>
2020-08-07 18:33:43 +0300

    replace logger

--- src/server/log/logger.cpp
+++ src/server/log/logger.cpp
@@ -2,14 +2,10 @@
 
 
 void init_logger() {
-    log4cplus::initialize();
+    boost::log::add_file_log("sample.log");
 
-    log4cplus::SharedAppenderPtr console_appender(new log4cplus::ConsoleAppender());
-    console_appender->setName(LOG4CPLUS_TEXT("Console_appender"));
-    log4cplus::Logger::getRoot().addAppender(console_appender);
-
-    log4cplus::SharedAppenderPtr file_appender(new log4cplus::RollingFileAppender(LOG4CPLUS_TEXT("Server.log"), 5 * 1024 * 1024, 5));
-    file_appender->setName(LOG4CPLUS_TEXT("File_appender"));
-    file_appender->setLayout(std::unique_ptr<log4cplus::Layout>(new log4cplus::PatternLayout(LOG4CPLUS_TEXT("%D{%d-%m-%Y %H:%M:%S.%q} [%-5p] <%x> %c - %m%n"))));
-    log4cplus::Logger::getRoot().addAppender(file_appender);
+    boost::log::core::get()->set_filter
+    (
+        boost::log::trivial::severity >= boost::log::trivial::info
+    );
 }
--- src/server/log/logger.h
+++ src/server/log/logger.h
@@ -1,14 +1,8 @@
 #pragma once
 
-#include "log4cplus/logger.h"
-#include "log4cplus/loggingmacros.h"
-#include "log4cplus/configurator.h"
-#include "log4cplus/fileappender.h"
-#include "log4cplus/layout.h"
-#include "log4cplus/ndc.h"
-#include "log4cplus/helpers/loglog.h"
-#include "log4cplus/consoleappender.h"
-
-#define LOGGER(x) log4cplus::Logger::getInstance(LOG4CPLUS_TEXT({x}))
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/utility/setup/file.hpp>
 
 void init_logger();
