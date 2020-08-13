603047fc23ead6a912d0da2d08921ed8e61d7fde
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-13 07:41:36 +0000

    Deploy k-morozov/AppChat to github.com/k-morozov/AppChat.git:gh-pages

--- search/all_3.js
+++ search/all_3.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['get_5fclient_5fid',['get_client_id',['../classConnection.html#ada8c3ce99e005bce63c6cd86310ca5c8',1,'Connection::get_client_id()'],['../classISubscriber.html#a176e45383c29328e20381540798b6c90',1,'ISubscriber::get_client_id()']]],
   ['get_5fconnection',['get_connection',['../classConnectionManager.html#a6896c0b1ffd9841b5c2b844618a711bc',1,'ConnectionManager']]],
-  ['get_5flogin',['get_login',['../classClient.html#a45237af352a98321d0b76fe829303ab3',1,'Client::get_login()'],['../classConnection.html#a8b2755786350d889aea9ae86d939808c',1,'Connection::get_login()'],['../classISubscriber.html#a0ed9b660a420d28f3df6ee0a3dc2cc11',1,'ISubscriber::get_login()']]],
+  ['get_5flogin',['get_login',['../classClient.html#ac358e90e4facc4305d2f25a1f1e5892f',1,'Client::get_login()'],['../classConnection.html#a8b2755786350d889aea9ae86d939808c',1,'Connection::get_login()'],['../classISubscriber.html#a0ed9b660a420d28f3df6ee0a3dc2cc11',1,'ISubscriber::get_login()']]],
   ['get_5fmessage',['get_message',['../classMainWindow.html#a9211fbba6944deb725822766176b65a2',1,'MainWindow']]],
   ['get_5froom_5fid',['get_room_id',['../classChannel.html#a58611981652a5d1563d428ce33b4f536',1,'Channel::get_room_id()'],['../classIRoom.html#ac5024ea2eeadf2152d742da91e94fcbf',1,'IRoom::get_room_id()']]],
-  ['get_5froomid',['get_roomid',['../classControlRequest.html#a77c8f3841a28cd1a8e3eda650e9cc15f',1,'ControlRequest::get_roomid()'],['../classTransportRequest.html#abb50636ebfe3bb688397490310e961f7',1,'TransportRequest::get_roomid()'],['../classTransportResponse.html#a811c14479087a286808f95d84cb66083',1,'TransportResponse::get_roomid()']]],
+  ['get_5froomid',['get_roomid',['../classControlRequest.html#aee1f9f7b3bc9b2a00642a4e4c2477ee7',1,'ControlRequest::get_roomid()'],['../classTransportRequest.html#ace7658bcebd254e3eb1e6de684f41d01',1,'TransportRequest::get_roomid()'],['../classTransportResponse.html#a5f7fdf98458f3efcb2f8f60be4493cd9',1,'TransportResponse::get_roomid()']]],
   ['get_5ftext_5ffrom_5fgui',['get_text_from_gui',['../classControl.html#a9741b13feb51b92934217ca854691e2c',1,'Control']]]
 ];
